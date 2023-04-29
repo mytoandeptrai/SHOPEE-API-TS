0192bb92c0a8ad6d725a4922c484b925cda2eb95
Toan Toan <67469007+mytoandeptrai@users.noreply.github.com>
2023-04-29 13:46:02 +0700

    fix: change middleware voucher

--- src/api/v1/vouchers/index.ts
+++ src/api/v1/vouchers/index.ts
@@ -5,9 +5,9 @@ import * as controller from './controller';
 const router = Router();
 
 router.get('/', authMiddleware.protectRouteMiddleware, controller.getAllVouchers);
-router.get('/:voucherId', authMiddleware.protectRouteMiddleware, controller.getAllPublicVouchers);
+router.get('/discover', controller.getAllPublicVouchers);
 router.get('/my-voucher', authMiddleware.protectRouteMiddleware, controller.getVoucherOfUser);
-router.get('/discover', authMiddleware.protectRouteMiddleware, controller.getAllPublicVouchers);
+router.get('/:voucherId', authMiddleware.protectRouteMiddleware, controller.getSingleVoucher);
 router.post('/save', authMiddleware.protectRouteMiddleware, controller.saveVoucher);
 
 export default router;
